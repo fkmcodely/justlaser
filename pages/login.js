b58387f937e91db8a417df465c7fb9b1747f97ff
KEVINDEV <kevin.rivera@visual-limes.com>
2021-10-07 14:25:21 +0200

    [ADD] -> End login example page: useForm

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import Head from "next/head";
-import { Container, Grid, Header } from 'semantic-ui-react';
+import { useForm } from 'react-hook-form';
+import { 
+    Container, 
+    Form, 
+    Checkbox, 
+    Button, 
+    Grid, 
+    Header,
+    Image 
+} from 'semantic-ui-react';
 
 const login = () => {
     return (
@@ -26,14 +35,34 @@ const login = () => {
 };
 
 function LogInContainer({}) {
-
+    const {register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = request => console.log(request);
+    
     return(
         <Grid columns="16" className="log-in-container">
+            <Image 
+                src="http://172.26.0.114:8080/farhos/VAADIN/themes/farhos/logo_farhos.png" 
+                className="log-in-container-logo" 
+                alt="" 
+            />
             <Grid.Row>
                 <Grid.Column textAlign="center" width="16">
                     <Header as="h3" className="log-in-container__title">
                         Identificación de usuario
                     </Header>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column width="16">
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <Form.Field>
+                            <input placeholder='Usuario' {...register("user")}/>
+                        </Form.Field>
+                        <Form.Field>
+                            <input placeholder='Contraseña' {...register("password")}/>
+                        </Form.Field>
+                        <Button className="log-in-container__submit" fluid type='submit'>ACEPTAR</Button>
+                    </Form>
                 </Grid.Column>
             </Grid.Row>
         </Grid>
